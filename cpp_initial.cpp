3741c59603371454778c26375aa7dd690c71016a
jvlahadamis <vlahadam@ualberta.ca>
2024-03-01 17:21:32 -0700

    cpp and plau and minus prices

---
+++ cpp_initial.cpp
@@ -0,0 +1,92 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+
+
+
+int price_plus(){
+  
+
+for (int i = 0; i <x.nrow(); i++){
+  
+// above defined basic for loop for vector
+// define variables we need to use to calculate the price.
+
+double par = ;
+double yield_plus = ;
+double maturity_in_years = maturity_in_years;
+
+price_plus = price_plus(par, yield_plus, maturity_in_years);
+
+/// above are all given
+/// need to get NPV below but can't use RTL::bond in the cpp code
+}
+
+
+double price_plus(double future_value, double yield, double years) {
+  
+  double recent_value;
+  recent_value = future_value/(pow(1+ yield), years);
+  return recent_value;
+  
+  
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int price_minus(){
+  
+  
+  for (int i = 0; i <x.nrow(); i++){
+    
+    // above defined basic for loop for vector
+    // define variables we need to use to calculate the price.
+    
+    double maturity_in_years = maturity_in_years;
+    double yield_plus = ;
+    double yield_minus = ;
+    /// above are all given
+    /// need to get NPV below but can't use RTL::bond in the cpp code
+    
+    
+    NumericVector price_plus()
+      
+      
+      
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
